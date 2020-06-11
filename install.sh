1b79476738a59f1f15fcb6e8120659ecf05223b8
BetterRobotics <ben.fogarty@live.com>
2020-06-11 23:17:50 +1000

    Install script

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "BrewZilla Installing now......"
+
+
+echo "
+
+
+UPDATING SYSTEM"
+sudo apt-get update && sudo apt-get upgrade -y
+
+echo "
+
+
+Installing needed packages"
+sudo apt-get install python-tk python-numpy -y
+pip install pyserial
+
+
+echo "
+
+
+Setting up devices"
+sudo bash -c 'echo "SUBSYSTEMS=="usb", DRIVERS=="ch341", MODE=="0666", GROUP=="dialout", SYMLINK+="ARDUINO"" > /etc/udev/rules.d/99-arduino.rules'
+
+echo "
+
+
+Setting up autorun..."
+echo '@lxpanel --profile LXDE-pi
+@pcmanfm --desktop --profile LXDE-pi
+@xscreensaver -no-splash
+
+@python /home/pi/BrewZilla/brewzilla.py' > /home/pi/.config/lxsession/LXDE-pi/autostart
+
+
+echo "
+
+
+Done!. Please reboot now."
